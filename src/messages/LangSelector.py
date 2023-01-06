b7f5a142c3cbd3952add48971bfd22887634efc7
Mistayan <hellogan.castramere@gmail.com>
2023-01-06 15:03:10 +0100

    logique simplifié

--- src/messages/LangSelector.py
+++ src/messages/LangSelector.py
@@ -11,27 +11,29 @@ class LangSelector:
      Par défaut, la langue de l'os est instanciée
      """
     __lang = None
-    msgs: LangInterface = None
+    _msgs: LangInterface = None
 
     def __init__(self, lang=None):
         """
         :param lang: isinstance(LangInterface, lang)
         """
         if lang:
-            self.msgs = lang() or lang  # Instantiate if it has not been passed instantiated
+            self._msgs = lang() or lang  # Instantiate if it has not been passed instantiated
         else:
             lang, formatting = locale.getlocale()
-            self.__lang = lang
+            try:
+                self.__lang = lang.split('_')[0]
+            except:
+                self.__lang = None
+
             match self.__lang:
-                case "English_United States":
-                    self.msgs = English()
-                case "English_Great Britain":
-                    self.msgs = English()
+                case "English":
+                    self._msgs = English()
                 case _:  # Défaut
-                    self.msgs = Francais()
+                    self._msgs = Francais()
 
     def __getattr__(self, item):
         """ Permet de récupérer les messages d'une langue (ayant la même nomination au travers des langues)"""
-        if self.msgs:
-            return self.msgs.__getattribute__(item)
+        if self._msgs:
+            return self._msgs.__getattribute__(item)
         raise AttributeError
