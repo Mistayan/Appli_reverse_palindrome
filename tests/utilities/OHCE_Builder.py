db4d22fb8f30891826d3d8fbf1a13fde50e62ca8
Mistayan <hellogan.castramere@gmail.com>
2023-01-06 15:04:52 +0100

    ajout clock au builder

--- tests/utilities/OHCE_Builder.py
+++ tests/utilities/OHCE_Builder.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-from messages import LangSelector, Francais
+from messages import Francais
 from models import OHCE
 
 
 class OHCEBuilder:
     _lang = Francais
+    _time: int
 
     def __init__(self):
         pass
 
     def build(self) -> OHCE.__class__:
-        return OHCE(self._lang)
+        return OHCE(lang=self._lang, time=self._time)
 
-    def prends_comme_langue(self, lang: LangSelector) -> OHCEBuilder:
+    def prends_comme_langue(self, lang) -> OHCEBuilder:
         """
         Permet d'assigner une langue à la volée
         :param lang: la langue souhaitée pour votre interface
@@ -21,4 +22,14 @@ class OHCEBuilder:
             >>> OHCEBuilder().prends_comme_langue(Francais).build()
         """
         self._lang = lang
+        return self
+
+    def a_heure_donnee(self, time: int):
+        """
+        Permet d'assigner une langue à la volée
+        :param time : l'heure souhaitée pour votre interface
+        :return : self, permet d'utiliser des instructions chainées:
+            >>> OHCEBuilder().a_heure_donnee(8).build()
+        """
+        self._time = time
         return self
