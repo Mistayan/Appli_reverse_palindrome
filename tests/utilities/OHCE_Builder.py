bec10ad872323853d8885b8301105f7a1e73188a
Mistayan <hellogan.castramere@gmail.com>
2023-01-06 15:41:41 +0100

    docstring structuration

--- tests/utilities/OHCE_Builder.py
+++ tests/utilities/OHCE_Builder.py
@@ -16,20 +16,21 @@ class OHCEBuilder:
 
     def prends_comme_langue(self, lang) -> OHCEBuilder:
         """
-        Permet d'assigner une langue à la volée
-        :param lang: la langue souhaitée pour votre interface
-        :return: self, permet d'utiliser des instructions chainées:  
+        Permet d'assigner une langue à la volée:
             >>> OHCEBuilder().prends_comme_langue(Francais).build()
+        :param lang: la langue souhaitée pour votre interface
+        :return: self, permet d'utiliser des instructions chainées
+
         """
         self._lang = lang
         return self
 
     def a_heure_donnee(self, time: int):
         """
-        Permet d'assigner une langue à la volée
-        :param time : l'heure souhaitée pour votre interface
-        :return : self, permet d'utiliser des instructions chainées:
+        Permet d'assigner une langue à la volée:
             >>> OHCEBuilder().a_heure_donnee(8).build()
+        :param time : l'heure souhaitée pour votre interface
+        :return : self, permet d'utiliser des instructions chainées.
         """
         self._time = time
         return self
