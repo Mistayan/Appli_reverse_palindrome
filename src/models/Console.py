a7b9bb775f83733c7bb7aa8dbcd9695e57a04cf4
Mistayan <hellogan.castramere@gmail.com>
2023-01-06 17:11:24 +0100

    made methods private

--- src/models/Console.py
+++ src/models/Console.py
@@ -10,12 +10,12 @@ class Console:
     @property
     def __main_menu(self):
         return {
-            "Lancer": self.ohce_,
+            "Lancer": self.__ohce_,
             # "Options": self.options,
-            "Quitter": self.quitter
+            "Quitter": self.__quitter
         }
 
-    def menu(self, _menu=None):
+    def __menu(self, _menu=None):
         _menu = _menu or self.__main_menu
         for i, (elem, action) in enumerate(_menu.items()):
             print(f"{i + 1} : {elem}")
@@ -28,14 +28,14 @@ class Console:
     def start(self):
         self._run = True
         while self._run:
-            self.menu()
+            self.__menu()
 
-    def ohce_(self):
+    def __ohce_(self):
         _in = input("Entrez un chaine de characters")
         print(self.__app.traiter(_in))
 
-    def options(self):
+    def __options(self):
         pass
 
-    def quitter(self):
+    def __quitter(self):
         self._run = False
