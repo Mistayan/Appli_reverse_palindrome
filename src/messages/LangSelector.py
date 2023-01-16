c90dfd71200d078ce7b1cdef1daf27fe2d8068f2
Mistayan <helixmastaz@gmail.com>
2023-01-16 21:04:39 +0100

    Update LangSelector.py
    added switch language possibilities de LangSelector

--- src/messages/LangSelector.py
+++ src/messages/LangSelector.py
@@ -1,6 +1,6 @@
 import locale
 
-from messages import LangInterface
+from src.messages import LangInterface
 from src.messages.English import English
 from src.messages.Francais import Francais
 
@@ -20,7 +20,7 @@ class LangSelector:
         if lang:
             self._msgs = lang() or lang  # Instantiate if it has not been passed instantiated
         else:
-            lang, formatting = locale.getlocale()
+            lang, formatting = locale.getlocale()  # WINDOWS FORMAT : English_Great Britain
             try:
                 self.__lang = lang.split('_')[0]
             except:
@@ -31,6 +31,22 @@ class LangSelector:
                     self._msgs = English()
                 case _:  # Défaut
                     self._msgs = Francais()
+
+    @property
+    def possibilities(self):
+        return {
+            "FR": Francais,
+            "EN": English,
+                }
+
+    def set_lang(self, _lang):
+        """
+         Définit la langue du sélecteur, par une action utilisateur
+        :param _lang: la langue sélectionnée par l'utilisateur, dans l'interface
+        :return: la phrase de sélection de la langue choisie
+        """
+        self._msgs = _lang or _lang()
+        return self._msgs.lang_select
 
     def __getattr__(self, item):
         """ Permet de récupérer les messages d'une langue (ayant la même nomination au travers des langues)"""
