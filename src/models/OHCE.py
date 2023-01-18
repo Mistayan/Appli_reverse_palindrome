e701b42fc9b3c0fe0f89280c456315046c6b4e2c
Mistayan <helixmastaz@gmail.com>
2023-01-18 18:49:36 +0100

    refacto: ajout sélecteur de message en fonction de l'heure (période) de la journée pour augmenter la scalabilité
    Signed-off-by: Mistayan <helixmastaz@gmail.com>

--- src/models/OHCE.py
+++ src/models/OHCE.py
@@ -16,48 +16,44 @@ class OHCE:
 
     @property
     def bonjour(self):
-        if self._time < 6:
-            msg_bonjour = self._lang.late_nighter
-        elif self._time < 12:
-            msg_bonjour = self._lang.bonjour
-        elif self._time < 16:
-            msg_bonjour = self._lang.bonjour
-        elif self._time < 20:
-            msg_bonjour = self._lang.bon_apres_midi
-        elif self._time < 24:
-            msg_bonjour = self._lang.bonsoir
-        else:
-            # if self._time >= 24 or self._time < 0:
-            raise ValueError("Invalid Time")
-        return msg_bonjour
+        _dict = {
+            # Avant HEURE, affiche TEXTE
+            6: self._lang.late_nighter,
+            12: self._lang.bonjour,
+            16: self._lang.bonjour,
+            20: self._lang.bon_apres_midi,
+            24: self._lang.bonsoir,
+        }
+        return self.__message_depend_d_heure(_dict)
 
     @property
     def au_revoir(self):
-        if self._time < 6:
-            msg_au_revoir = self._lang.de_bon_matin
-        elif self._time < 12:
-            msg_au_revoir = self._lang.bonne_journee
-        elif self._time < 16:
-            msg_au_revoir = self._lang.bon_apres_midi
-        elif self._time < 20:
-            msg_au_revoir = self._lang.bonne_soiree
-        elif self._time < 22:
-            msg_au_revoir = self._lang.bonne_nuit
-        elif self._time < 24:
-            msg_au_revoir = self._lang.vas_te_coucher
-        else:
-            # if _time >= 24 or _time < 0:
-            raise ValueError("Invalid Time")
-        return msg_au_revoir
+        _dict = {
+            # Avant HEURE, affiche TEXTE
+            6: self._lang.de_bon_matin,
+            12: self._lang.bonne_journee,
+            16: self._lang.bon_apres_midi,
+            20: self._lang.bonne_soiree,
+            22: self._lang.bonne_nuit,
+            24: self._lang.vas_te_coucher,
+        }
+        return self.__message_depend_d_heure(_dict)
+
+    def __message_depend_d_heure(self, _dict: dict) -> str:
+        """ En fonction du dictionnaire reçu et de l'heure actuelle, renvoi le message adapté"""
+        for heure, texte in _dict.items():
+            if 0 <= self._time < heure:
+                return texte
+        raise ValueError("Invalid Time")
 
     @staticmethod
-    def string_palindrome(string: str):
+    def miroir(string: str):
         return string[::-1]
 
     def traiter(self, string: str):
         if not isinstance(string, str):
             raise ValueError(self._lang.impossible_reverse)
-        _reversed = self.string_palindrome(string)
+        _reversed = self.miroir(string)
         if string.lower() == "radar":
             string = string[::-1]
         bien_dit = self._lang.bien_dit + '\n' if _reversed == string else ''
