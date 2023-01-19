554b274b806cd7375cf5d8aeac4c2ea38e0910f2
Mistayan <helixmastaz@gmail.com>
2023-01-19 08:05:21 +0100

    ajout phrases d'erreur sur les asserts
    Signed-off-by: Mistayan <helixmastaz@gmail.com>

--- tests/test_02_OHCE_Integration.py
+++ tests/test_02_OHCE_Integration.py
@@ -25,13 +25,16 @@ class OHCEIntegrationTests(unittest.TestCase):
         self.assertRegex(resultat, r"^{}".format(lang.bonsoir), "OHCE doit dire bonjour")
 
         # PUIS son palindrome est imprimé
-        self.assertRegex(resultat, r"^{}\n{}\n".format(lang.bonsoir, "YEY"))
+        self.assertRegex(resultat, r"^{}\n{}\n".format(lang.bonsoir, "YEY"),
+                         "Le palindrome doit se trouver après bonjour")
 
         # PUIS il est félicité
-        self.assertRegex(resultat, r"\n{}\n{}\n".format("YEY", lang.bien_dit))
+        self.assertRegex(resultat, r"\n{}\n{}\n".format("YEY", lang.bien_dit),
+                         "Les félicitations pour un palindrome doit se trouver après le mot")
 
         # PUIS on lui dit bonne nuit
-        self.assertRegex(resultat, r"\n{}\n{}$".format(lang.bien_dit, lang.bonne_nuit))
+        self.assertRegex(resultat, r"\n{}\n{}$".format(lang.bien_dit, lang.bonne_nuit),
+                         "le message d'au revoir doit se trouver après bien_dit")
 
     def test_scenarii_non_palindrome_matin_fr(self):
         """
@@ -50,8 +53,10 @@ class OHCEIntegrationTests(unittest.TestCase):
         # ALORS il est salué
         self.assertRegex(resultat, r"^{}\n".format(lang.bonjour), "OHCE doit dire bonjour en premier")
 
-        # PUIS son palindrome est imprimé
-        self.assertRegex(resultat, r"^{}\n{}\n".format(lang.bonjour, "YEH"))
+        # PUIS son miroir est imprimé
+        self.assertRegex(resultat, r"^{}\n{}\n".format(lang.bonjour, "YEH"),
+                         "Le miroir du mot doit se trouver après bonjour")
 
         # PUIS on lui souhaite une bonne journée
-        self.assertRegex(resultat, r"\n{}\n{}$".format("YEH", lang.bonne_journee))
+        self.assertRegex(resultat, r"\n{}\n{}$".format("YEH", lang.bonne_journee),
+                         "Le message d'au_revoir doit se trouver après le mot en miroir et à la fin")
